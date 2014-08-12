c761eace0aee7df5e455654f17a4028ad38ec94f
Rudy <rudycodes@gmail.com>
2014-08-12 21:09:07 +0500

    made specs pass

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,24 @@ class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :comapny_name, :email
   
   has_many :notes
+  def self.merge(winner_obj,looser_obj)
+    if !winner_obj.notes.nil? && !looser_obj.notes.nil? 
+      winner_obj.email = looser_obj.email unless winner_obj.email.nil?
+      winner_obj.notes<<looser_obj.notes
+      looser_obj.delete
+      winner_obj
+    end
+  end
+
+  def duplicates?(dup_obj)
+    if dup_obj==nil
+      false
+    elsif dup_obj.first_name == self.first_name && 
+      dup_obj.last_name ==self.last_name && 
+      dup_obj.email = self.email
+      true
+    else
+      false 
+    end  
+  end
 end
